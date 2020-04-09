1d11aa3c6987026745c20b264684365c5d777121
Tanner Stirrat <tstirrat@gmail.com>
2020-04-09 09:37:55 -0600

    Add some tests and make sure they pass (#12)
    * Add some tests and make sure they pass
    
    * Add boto for testing things

--- covid-csv-filter/lambda_test.py
+++ covid-csv-filter/lambda_test.py
@@ -1,3 +1,44 @@
-def test_pytest_is_working():
-    # This is a comment
-    pass
+from lambda_function import (
+    state_stream,
+    county_stream,
+    STATE,
+    COUNTY,
+    DEATHS,
+    POPULATION,
+    CASES,
+    COLORADO,
+    GARFIELD,
+    GARFIELD_POPULATION,
+)
+
+
+def test_state_stream_filters_out_non_colorado_states():
+    stream = [
+        {STATE: COLORADO, DEATHS: 1, CASES: 1},
+        {STATE: 'not-colorado', DEATHS: 1, CASES: 1},
+    ]
+    assert len([state_stream(stream)]) == 1
+
+
+def test_state_stream_adds_population_field():
+    stream = [
+        {STATE: COLORADO, DEATHS: 1, CASES: 1},
+    ]
+    assert next(state_stream(stream))[POPULATION] != 0
+
+
+def test_county_stream_filters_out_non_county_entries():
+    stream = [
+        {COUNTY: GARFIELD, STATE: COLORADO, DEATHS: 1, CASES: 1},
+        {COUNTY: GARFIELD, STATE: 'not-colorado', DEATHS: 1, CASES: 1},
+        {COUNTY: 'not-garfield', STATE: COLORADO, DEATHS: 1, CASES: 1},
+        {COUNTY: 'not-garfield', STATE: 'not-colorado', DEATHS: 1, CASES: 1},
+    ]
+    assert len([county_stream(GARFIELD, GARFIELD_POPULATION)(stream)]) == 1
+
+
+def test_county_stream_adds_population_field():
+    stream = [
+        {COUNTY: GARFIELD, STATE: COLORADO, DEATHS: 1, CASES: 1},
+    ]
+    assert next(county_stream(GARFIELD, GARFIELD_POPULATION)(stream)) != 0
